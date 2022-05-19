d1e322acc4d64cbf890e7c95b440a7698d1e38fa
The-Professor99 <festusihechi99@gmail.com>
2022-05-19 19:56:15 +0100

    Modify sidebar

--- src/app/shared-components/shared.module.ts
+++ src/app/shared-components/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 import { TopbarComponent } from './topbar/topbar.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [SidebarComponent, TopbarComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatButtonModule],
   exports: [SidebarComponent, CommonModule],
 })
 export class SharedModule {}
